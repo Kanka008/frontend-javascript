19df0b4c0a5749759d96e7fe59bf00b608b210a0
Monicah <gacherumonicah7@gmail.com>
2025-06-17 12:13:33 +0300

    Advanced types Part 1

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -33,3 +33,11 @@ export class Teacher implements TeacherInterface{
         return `Getting to work.`
     }
 }
+
+export function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
