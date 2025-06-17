a064d8c9cf9a84dfe3bcef30abeea6c82c60c203
Monicah <gacherumonicah7@gmail.com>
2025-06-17 05:31:59 +0300

    Saving updates

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,9 @@
+var teacher1 = {
+    firstName: "Marion",
+    lastName: "Mituri",
+    fullTimeEmployee: true,
+    yearsOfExperience: 5,
+    location: "Kiambogo",
+    contract: 5,
+};
+console.log(teacher1);
