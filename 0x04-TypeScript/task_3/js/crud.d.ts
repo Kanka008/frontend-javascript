adf92f7b9bd967cdd3178d63108d38160baf53ad
Monicah <gacherumonicah7@gmail.com>
2025-06-18 12:10:04 +0300

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,10 +1,8 @@
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud';
 
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
-export function updatedRow(newRowID: number, updatedRow: RowElement) {
-    throw new Error('Function not implemented.');
-}
+export declare function insertRow(row: RowElement): number;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): number;
+
 
