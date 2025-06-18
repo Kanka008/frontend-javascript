9ab722d0a353758235574a280ff47e08ba02ab33
Monicah <gacherumonicah7@gmail.com>
2025-06-18 23:18:26 +0300

    Update task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,12 +1,11 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
 namespace Subjects{
     export interface Teacher{
         experienceTeachingJava?: number;
     }
-    export class Java{
-        teacher:Teacher;
-        constructor(teacher:Teacher){
-            this.teacher = teacher;
-        }
+    export class Java extends Subject{
         getRequirements(): string{
             return`Here is the list of requirements for React`
         }
--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,17 +1,16 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
 namespace Subjects{
     export interface Teacher{
         experienceTeachingReact?: number;
     }
-    export class React{
-        teacher:Teacher;
-        constructor(teacher:Teacher){
-            this.teacher = teacher;
-        }
+    export class React extends Subject{
         getRequirements(): string{
             return`Here is the list of requirements for React`
         }
         getAvailableTeacher(): string{
-            if(this.teacher.experienceTeachingReact &&this.teacher.experienceTeachingReact > 0 ){
+            if(this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0 ){
                 return `AvailableTeacher:${this.teacher.firstName}`
             }else{
                 return`No available teacher`
